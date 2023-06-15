84071b9b1d15fe16947af4351748efb11e7f4b0e
Karthik P N <karthikpn@Karthiks-MBP.ef.technion.ac.il>
2023-06-15 16:14:00 +0300

    Updates

---
+++ precache-manifest.c332bc96cd781b35d31761162064132c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c84686bcf222e01110c2",
+    "url": "/static/css/main.b9788048.chunk.css"
+  },
+  {
+    "revision": "c84686bcf222e01110c2",
+    "url": "/static/js/main.c84686bc.chunk.js"
+  },
+  {
+    "revision": "018e9a16b8f581b3b248",
+    "url": "/static/js/1.018e9a16.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3439e9b83f5ed22bec77bd8422a2ec0e",
+    "url": "/index.html"
+  }
+];
