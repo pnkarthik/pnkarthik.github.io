ea8a24b6c69100f31bd3db0f33301b8a9a7afc03
P. N. Karthik <karthikpn@Ps-MacBook-Pro.local>
2023-11-28 16:19:14 +0800

    Updates

---
+++ precache-manifest.5e450f9e6126cd257f7b8d1686091e8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e2a3a0c3c52538f166332c8e05ffa23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f50add214bf4b5efb8c",
+    "url": "/static/css/main.66d9fcfd.chunk.css"
+  },
+  {
+    "revision": "ce30e814242dd9f1bcb8",
+    "url": "/static/js/2.4e1f8a82.chunk.js"
+  },
+  {
+    "revision": "8f50add214bf4b5efb8c",
+    "url": "/static/js/main.3fefcf47.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
