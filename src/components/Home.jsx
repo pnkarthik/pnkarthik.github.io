73cb4bac5530054fa41d06ba643b41955ae002d8
Srividya BV <srividya.bv@sakhaglobal.com>
2024-12-21 21:19:05 +0530

    adding no interns message

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,9 +6,6 @@ const Home = () => {
   return (
     <>
       <div className="row">
-        {/* <div className="col-md-2">
-          <hr className="headingBar col-md-8" />
-        </div> */}
         <div className="col-md-12">
           <div className="headingFontSize"><span>About</span><span>Me</span></div>
           <p className="contentFontSize">
@@ -21,10 +18,10 @@ const Home = () => {
           </p>
         </div>
       </div>
+      <div className="border border-2 border-danger p-2 fs-5 mb-3">
+        I do not consider interns from outside IIT Hyderabad. Please do not feel offended if I do not reply to your internship request.
+      </div>
       <div className="row alignBaseline">
-        {/* <div className="col-md-2">
-          <hr className="headingBar col-md-8" />
-        </div> */}
         <div className="col-md-12">
           <div className="headingFontSize"><span>Research</span><span>Interests</span></div>
           <p className="contentFontSize">Multi-armed bandits, statistical learning, federated learning, transfer learning, information theory, sequential analysis, hypothesis testing, Markov decision processes, anomaly detection, stochastic adaptive control</p>
