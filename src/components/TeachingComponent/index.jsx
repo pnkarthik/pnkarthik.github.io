50a645058d59560d940f65fcabd0c029d5a128f0
Karthik P N <karthikpn@Karthiks-Laptop.local>
2025-05-24 17:06:31 +0530

    course feedback comments

--- src/components/TeachingComponent/index.jsx
+++ src/components/TeachingComponent/index.jsx
@@ -6,20 +6,6 @@ const TeachingComponent = () => {
   return (
     <>
       <div className="col">
-      From the earliest days of my life, teaching has been a calling that I could not ignore. Whether I was explaining hard mathematical concepts to my sibling or watching in awe as my own teachers brought complex ideas to life in classroom, I knew that I wanted to be a part of this transformative process called teaching. In my experience, effective teaching is about more than just delivering information. To truly inspire and motivate students, it is essential to stay up‑to‑date with the latest developments in one’s field and to approach teaching as a form of ongoing research.
-      </div>
-      <div className="col">
-        <p></p>
-        <p className="headingFontSize">My Teaching Philosophy</p>
-          My teaching philosophy is rooted in the belief that all students can learn, given a conducive learning environment and an engaging teaching style that caters to their unique needs. I believe that teaching is a two‑way process, where both students and teachers learn from one another. Below is a glimpse into my modus operandi of teaching.
-        <ul>
-          <li>Teaching After Learning, Learning After teaching:<br/>I believe that teaching should follow learning and learning should follow teaching. Prior to delivering a lecture on a particular topic, I introspect about ways to explain the concept in the simplest possible terms to someone unfamiliar with the subject matter. Once I have arrived at a clear and concise explanation that is satisfactory to me, I deliver the same in the lecture while keeping open my thought process (you read that right; listening to lectures is a great way to delve into the thought process of the person delivering the lecture). At regular intervals of time, I solicit feedback on my teaching; this allows me to identify areas where my explanations can be refined and improved.</li>
-          <li>Teaching as a Tool to Kindle the Spirit of Inquiry:<br/>My exposure to lectures delivered by distinguished researchers has reinforced my core belief that effective teaching involves instilling in students an intrinsic curiosity and a capacity for inquiry, rather than merely providing them with ready-made solutions. Drawing on the pedagogical practices of my mentors, I strive to maintain a delicate balance between delivering well-crafted material that is easily comprehensible, while also offering points of reflection that foster students' cognitive development.</li>
-          <li>Undergraduate Teaching Philosophy:<br/>The undergraduate student community in India is renowned for its exceptional intellectual capabilities, cultivated through rigorous training for entrance exams such as the Joint Entrance Exam (JEE). To capitalize on this strength, it is imperative to impart to these students the art of breaking down complex problems into smaller, manageable tasks, and approach each one of them systematically. To achieve this goal, I typically introduce problem sets which comprise a number of smaller sub-problems, upon solving all of which the overall problem will be solved. I try to present live simulations in class, allowing students to observe the source code and witness first-hand how simple concepts can be visualised. For instance, in <a href="https://youtu.be/xyjWP0GQD0U">this lecture</a> delivered to the undergraduate students at R V College of Engineering, I demonstrate using Python simulations how a Gamma distribution can be fitted to the data obtained from a certain "odd-one-out" task in cognitive neuroscience, and how the goodness of fit can be assessed using Kolmogorov--Smirnoff tests. This illustrates how everyday phenomena can be quantitatively analyzed using fundamental mathematical tools. By presenting such real-world examples in class, I attempt to motivate students to explore and apply mathematical concepts in their own problem-solving endeavors.</li>
-          <li>Graduate Teaching Philosophy:<br/>While undergraduate education is a crucial foundation for honing a student's problem-solving aptitude, typically emphasizing "how" things work, graduate studies offers a switches of perspective to understand "why" things work the way they work. I believe that it is crucial for graduate students to have the ability to assimilate complex ideas from a variety of sources and apply them effectively to solve problems. To reinforce this essential skill, I typically design questions that guide students to think systematically, by breaking bigger problems to smaller sub-problems and building on the solution of one sub-problem to solve another sub-problem or a different problem.</li>
-        </ul>
-      </div>
-      <div className="col">
         <p></p>
         <p className="headingFontSize">Teaching at IIT Hyderabad</p>
         <div className="w-100 max-w-100 overflow-auto">
@@ -27,11 +13,12 @@ const TeachingComponent = () => {
             <thead>
               <tr>
                 <th className="py-2 px-4">Term</th>
-                <th className="py-2 px-4">Course Code</th>
-                <th className="py-2 px-4">Course Title</th>
-                <th className="py-2 px-4">No of Students</th>
+                <th className="py-2 px-4">Code</th>
+                <th className="py-2 px-4">Title</th>
+                <th className="py-2 px-4">Enrolment</th>
                 <th className="py-2 px-4">Course Rating</th>
                 <th className="py-2 px-4">Instructor Rating</th>
+                <th className="py-2 px-4">Feedback</th>
               </tr>
             </thead>
             <tbody>
@@ -56,6 +43,11 @@ const TeachingComponent = () => {
                   <td className="py-2 px-4">{data?.noOfStudents}</td>
                   <td className="py-2 px-4">{data?.courseRating}</td>
                   <td className="py-2 px-4">{data?.instructorRating}</td>
+                  <td className="py-2 px-4">
+                    <a href={data?.feedback} target="_blank" rel="noopener noreferrer">
+                      Comments
+                    </a>
+                  </td>
                 </tr>
               ))}
             </tbody>
@@ -111,6 +103,17 @@ const TeachingComponent = () => {
             </tbody>
           </table>
         </div>
+      </div>
+      <div className="col">
+        <p></p>
+        <p className="headingFontSize">My Teaching Philosophy</p>
+          My teaching philosophy is rooted in the belief that all students can learn, given a conducive learning environment and an engaging teaching style that caters to their unique needs. I believe that teaching is a two‑way process, where both students and teachers learn from one another. Below is a glimpse into my modus operandi of teaching.
+        <ul>
+          <li>Teaching After Learning, Learning After teaching:<br/>I believe that teaching should follow learning and learning should follow teaching. Prior to delivering a lecture on a particular topic, I introspect about ways to explain the concept in the simplest possible terms to someone unfamiliar with the subject matter. Once I have arrived at a clear and concise explanation that is satisfactory to me, I deliver the same in the lecture while keeping open my thought process (you read that right; listening to lectures is a great way to delve into the thought process of the person delivering the lecture). At regular intervals of time, I solicit feedback on my teaching; this allows me to identify areas where my explanations can be refined and improved.</li>
+          <li>Teaching as a Tool to Kindle the Spirit of Enquiry:<br/>My exposure to lectures delivered by distinguished researchers has reinforced my core belief that effective teaching involves instilling in students an intrinsic curiosity and a capacity for enquiry, rather than merely providing them with ready-made solutions. Drawing on the pedagogical practices of my mentors, I strive to maintain a delicate balance between delivering well-crafted material that is easily comprehensible, while also offering points of reflection that foster students' cognitive development.</li>
+          <li>Undergraduate Teaching Philosophy:<br/>The undergraduate student community in India is renowned for its exceptional intellectual capabilities, cultivated through rigorous training for entrance exams such as the Joint Entrance Exam (JEE). To capitalize on this strength, it is imperative to impart to these students the art of breaking down complex problems into smaller, manageable tasks, and approach each one of them systematically. To achieve this goal, I typically introduce problem sets which comprise a number of smaller sub-problems, upon solving all of which the overall problem will be solved. I try to present live simulations in class, allowing students to observe the source code and witness first-hand how simple concepts can be visualised. For instance, in <a href="https://youtu.be/xyjWP0GQD0U">this lecture</a> delivered to the undergraduate students at R V College of Engineering, I demonstrate using Python simulations how a Gamma distribution can be fitted to the data obtained from a certain "odd-one-out" task in cognitive neuroscience, and how the goodness of fit can be assessed using Kolmogorov--Smirnoff tests. This illustrates how everyday phenomena can be quantitatively analyzed using fundamental mathematical tools. By presenting such real-world examples in class, I attempt to motivate students to explore and apply mathematical concepts in their own problem-solving endeavors.</li>
+          <li>Graduate Teaching Philosophy:<br/>While undergraduate education is a crucial foundation for honing a student's problem-solving aptitude, typically emphasizing "how" things work, graduate studies offers a switches of perspective to understand "why" things work the way they work. I believe that it is crucial for graduate students to have the ability to assimilate complex ideas from a variety of sources and apply them effectively to solve problems. To reinforce this essential skill, I typically design questions that guide students to think systematically, by breaking bigger problems to smaller sub-problems and building on the solution of one sub-problem to solve another sub-problem or a different problem.</li>
+        </ul>
       </div>
     </>
   );
